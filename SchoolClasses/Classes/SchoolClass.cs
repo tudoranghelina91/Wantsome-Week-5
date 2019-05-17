9fdb48438cb241471435bf2248b6f52557adb38c
Tudor Anghelina <tudor.anghelina91@gmail.com>
2019-05-17 18:01:27 +0300

    Updated SchoolClass to set name as "CL" + ID value

--- SchoolClasses/Classes/SchoolClass.cs
+++ SchoolClasses/Classes/SchoolClass.cs
@@ -13,10 +13,11 @@ namespace SchoolClasses.Classes
         public List<Student> Students { get; set; } = new List<Student>();
         public List<Teacher> Teachers { get; set; } = new List<Teacher>();
         public string Comments { get; set; }
+        static int ID = 0;
 
-        public SchoolClass(string name)
+        public SchoolClass()
         {
-            Name = name;
+            Name = "CL" + (++ID);
         }
     }
 }
