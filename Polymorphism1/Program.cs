bf5d7f5b8effbfa239b6cfd3eddaa200bb591a8f
Tudor Anghelina <tudor.anghelina91@gmail.com>
2019-05-17 20:13:57 +0300

    Added Polymorphism code from class

---
+++ Polymorphism1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism1
+{
+    class Program
+    {
+        public interface IArea
+        {
+            double GetArea();
+        }
+
+        public class Figura
+        {
+            public Figura()
+            {
+
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            // override
+
+            // overload
+        }
+    }
+}
