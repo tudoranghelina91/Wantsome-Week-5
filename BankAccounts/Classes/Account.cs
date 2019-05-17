b478bc35297bfbefc1d9d452ebbc53228acf3bc3
Tudor Anghelina <tudor.anghelina91@gmail.com>
2019-05-17 23:10:11 +0300

    Added IDeposit & IWithdraw interfaces, created Account base class and Deposit Account derived class

---
+++ BankAccounts/Classes/Account.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccounts.Classes
+{
+    abstract class Account
+    {
+        public enum Customer { Individual, Company };
+        public float Balance { get; set; }
+        public float InterestRate { get; set; }
+
+        public virtual float CalculateInterest(int numberOfMonths)
+        {
+            return numberOfMonths * InterestRate;
+        }
+    }
+}
---
+++ BankAccounts/Interfaces/IWithdraw.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccounts.Interfaces
+{
+    interface IWithdraw
+    {
+        void Withdraw(float amount);
+    }
+}
