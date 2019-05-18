00266812f5cef72f2265d0191f4c6f985396717f
Tudor Anghelina <tudor.anghelina91@gmail.com>
2019-05-18 22:43:07 +0300

    Created Class Diagram for BusinessModel

--- BusinessModel/Program.cs
+++ BusinessModel/Program.cs
@@ -14,13 +14,19 @@ namespace BusinessModel
         static void Main(string[] args)
         {
             IPerson person = new Customer("Alex");
+
             person.PlaceOrder(StoresStruct.FordStore, CarsStruct.FordFocus);
+
             Console.WriteLine("Orders after ordering from Ford Store");
             person.Print();
+
             person.PlaceOrder(StoresStruct.SkodaStore, CarsStruct.FordFocus);
+
             Console.WriteLine("Orders after ordering new model from Skoda Store");
             person.Print();
+
             person.CancelOrder(StoresStruct.FordStore, person.Orders.Find(o => o.Vehicle.Model == CarsStruct.FordFocus.Model));
+
             Console.WriteLine("Orders after cancelling the order on Ford Store");
             person.Print();
             Console.ReadKey();
